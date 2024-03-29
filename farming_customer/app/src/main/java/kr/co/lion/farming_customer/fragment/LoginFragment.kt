0e9de387d7c9eb00c0ccc853acb493233819552e
Kyung Soo Lee <blueeye0909@naver.com>
2024-03-29 16:01:22 +0900

    :sparkles: Feat LoginFragment
    로그인 버튼, 비회원으로 시작하기 버튼 누를 시 Home화면으로 넘어가도록 설정
    related to: #4

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/LoginFragment.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/LoginFragment.kt
@@ -1,5 +1,6 @@
 package kr.co.lion.farming_customer.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -25,6 +26,38 @@ class LoginFragment : Fragment() {
         fragmentLoginBinding.loginViewModel = loginViewModel
         fragmentLoginBinding.lifecycleOwner = this
 
+        loginActivity = activity as LoginActivity
+
+        settingButtonLogin()
+        settingButtonLoginWithoutReg()
+
         return fragmentLoginBinding.root
+    }
+
+    // 로그인 버튼 클릭 시
+    fun settingButtonLogin(){
+        fragmentLoginBinding.apply {
+            buttonLoginLogin.setOnClickListener {
+
+                // 일단 홈화면으로 넘어간다.
+                val intent = Intent(loginActivity, MainActivity::class.java)
+                startActivity(intent)
+                loginActivity.finish()
+                TODO("나중엔 DB 연결")
+            }
+        }
+    }
+
+    // 비회원으로 로그인 버튼 관련 함수
+    fun settingButtonLoginWithoutReg(){
+        fragmentLoginBinding.apply {
+
+            // 버튼 클릭 시 MainActivity를 보여준다.
+            buttonLoginWithoutReg.setOnClickListener {
+                val intent = Intent(loginActivity, MainActivity::class.java)
+                startActivity(intent)
+                loginActivity.finish()
+            }
+        }
     }
 }
