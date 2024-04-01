fff54d866802256723573afd8801f4c9a30f1dbc
Kyung Soo Lee <blueeye0909@naver.com>
2024-04-01 17:07:16 +0900

    :lipstick: Add 회원가입 완료 화면
    회원가입 완료 화면 구성
    related to:#4

---
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/Register3Fragment.kt
@@ -0,0 +1,22 @@
+package kr.co.lion.farming_customer.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.co.lion.farming_customer.activity.LoginActivity
+import kr.co.lion.farming_customer.databinding.FragmentRegister3Binding
+import kr.co.lion.farming_customer.viewmodel.Register3ViewModel
+
+class Register3Fragment : Fragment() {
+
+    lateinit var fragmentRegister3Binding: FragmentRegister3Binding
+    lateinit var loginActivity: LoginActivity
+
+    lateinit var register3ViewModel: Register3ViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+    }
+}
