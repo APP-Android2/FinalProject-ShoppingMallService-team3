ed0fe599e2958f4cfedd8936b3799438e4b4c5a0
Jieun <lje2000lje@naver.com>
2024-04-04 16:21:02 +0900

    :sparkles: Add FAQ 어댑터, 아코디언 구현
    Related to : #10

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/myPageServiceCenter/MyPageServiceCenterFaqFragment.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/myPageServiceCenter/MyPageServiceCenterFaqFragment.kt
@@ -6,16 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.lion.farming_customer.R
-import kr.co.lion.farming_customer.activity.MainActivity
 import kr.co.lion.farming_customer.activity.myPageServiceCenter.MyPageServiceCenterActivity
 import kr.co.lion.farming_customer.databinding.FragmentMyPageServiceCenterFaqBinding
+import kr.co.lion.farming_customer.fragment.myPageServiceCenter.adapter.FaqRVAdapter
 import kr.co.lion.farming_customer.viewmodel.myPageServiceCenter.ServiceCenterFaqViewModel
 
 class MyPageServiceCenterFaqFragment : Fragment() {
     lateinit var binding : FragmentMyPageServiceCenterFaqBinding
     lateinit var myPageServiceCenterActivity: MyPageServiceCenterActivity
     lateinit var viewModel : ServiceCenterFaqViewModel
+
+    lateinit var faqRVAdapter : FaqRVAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +29,10 @@ class MyPageServiceCenterFaqFragment : Fragment() {
         binding.serviceCenterFaqViewModel = viewModel
         binding.lifecycleOwner = this
         myPageServiceCenterActivity = activity as MyPageServiceCenterActivity
+
+        faqRVAdapter = FaqRVAdapter()
+        binding.serviceCenterFaqRv.adapter = faqRVAdapter
+        binding.serviceCenterFaqRv.layoutManager = LinearLayoutManager(context)
 
         return binding.root
     }
