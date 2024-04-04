f9b917f99660c9166dc1b24d0100015b1f06b903
inwoong <525inwung@gmail.com>
2024-04-04 16:48:26 +0900

    :lipstick: Update 레이팅 바 라이브러리 적용 기존 사용했던 스타일 및 레이아웃 파일 삭제
    Related to #5

---
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/viewmodel/farmingLife/rowFarmingLifeReviewViewModel.kt
@@ -0,0 +1,14 @@
+package kr.co.lion.farming_customer.viewmodel.farmingLife
+
+import androidx.lifecycle.MutableLiveData
+
+class rowFarmingLifeReviewViewModel {
+    val textView_name = MutableLiveData<String>()
+    val rating = MutableLiveData<Double>()
+
+    val textView_reviewDate = MutableLiveData<String>()
+
+    val textView_productName = MutableLiveData<String>()
+    val textView_optionName = MutableLiveData<String>()
+    val textView_content = MutableLiveData<String>()
+}
