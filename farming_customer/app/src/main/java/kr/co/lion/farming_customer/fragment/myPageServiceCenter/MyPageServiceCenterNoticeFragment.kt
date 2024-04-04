3bd06aab6735a474c87e54daa135ab6b04bdd484
Jieun <lje2000lje@naver.com>
2024-04-04 15:31:02 +0900

    :sparkles: Add 공지사항 어댑터 연결
    공지사항 리사이클러뷰, 아코디언 구현 Related to : #10

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/myPageServiceCenter/MyPageServiceCenterNoticeFragment.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/myPageServiceCenter/MyPageServiceCenterNoticeFragment.kt
@@ -6,15 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.lion.farming_customer.R
 import kr.co.lion.farming_customer.activity.myPageServiceCenter.MyPageServiceCenterActivity
 import kr.co.lion.farming_customer.databinding.FragmentMyPageServiceCenterNoticeBinding
+import kr.co.lion.farming_customer.fragment.myPageServiceCenter.adapter.NoticeRVAdapter
 import kr.co.lion.farming_customer.viewmodel.myPageServiceCenter.ServiceCenterNoticeViewModel
 
 class MyPageServiceCenterNoticeFragment : Fragment() {
     lateinit var binding: FragmentMyPageServiceCenterNoticeBinding
     lateinit var myPageServiceCenterActivity: MyPageServiceCenterActivity
     lateinit var viewModel: ServiceCenterNoticeViewModel
+
+    lateinit var noticeRVAdapter: NoticeRVAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +29,10 @@ class MyPageServiceCenterNoticeFragment : Fragment() {
         binding.serviceCenterNoticeViewModel = viewModel
         binding.lifecycleOwner = this
         myPageServiceCenterActivity = activity as MyPageServiceCenterActivity
+
+        noticeRVAdapter = NoticeRVAdapter()
+        binding.serviceCenterNoticeRv.adapter = noticeRVAdapter
+        binding.serviceCenterNoticeRv.layoutManager = LinearLayoutManager(context)
 
         return binding.root
     }
