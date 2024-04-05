d1a664ccf2079a570a68214257e69346de836f90
leesuwon0673 <lsw0398@naver.com>
2024-04-05 11:31:35 +0900

    💄 Add 좋아요 UI cardView 구현
    cardView 구현만 했습니다 Related : #15

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/LikeFragment.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/LikeFragment.kt
@@ -6,9 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import kr.co.lion.farming_customer.R
 import kr.co.lion.farming_customer.activity.MainActivity
 import kr.co.lion.farming_customer.databinding.FragmentLikeBinding
+import kr.co.lion.farming_customer.databinding.RowLikeBinding
 import kr.co.lion.farming_customer.viewmodel.LikeViewModel
 
 class LikeFragment : Fragment() {
@@ -23,7 +28,48 @@ class LikeFragment : Fragment() {
         fragmentLikeBinding.lifecycleOwner = this
         mainActivity = activity as MainActivity
 
+        settingRecyclerViewLike()
+
         return fragmentLikeBinding.root
+    }
+
+    // recyclerView 설정
+    fun settingRecyclerViewLike(){
+        fragmentLikeBinding.apply {
+            recyclerViewLike.apply {
+                adapter = LikeRecyclerViewAdapter()
+                layoutManager = GridLayoutManager(mainActivity,2)
+            }
+        }
+    }
+
+    // recyclerView의 Adapter
+    inner class LikeRecyclerViewAdapter:RecyclerView.Adapter<LikeRecyclerViewAdapter.LikeViewHolder>(){
+        inner class LikeViewHolder(rowLikeBinding:RowLikeBinding):RecyclerView.ViewHolder(rowLikeBinding.root){
+            val rowLikeBinding:RowLikeBinding
+
+            init {
+                this.rowLikeBinding = rowLikeBinding
+                this.rowLikeBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LikeViewHolder {
+            val rowLikeBinding = RowLikeBinding.inflate(layoutInflater)
+            val likeViewHolder = LikeViewHolder(rowLikeBinding)
+            return likeViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 9
+        }
+
+        override fun onBindViewHolder(holder: LikeViewHolder, position: Int) {
+
+        }
     }
 
 }
