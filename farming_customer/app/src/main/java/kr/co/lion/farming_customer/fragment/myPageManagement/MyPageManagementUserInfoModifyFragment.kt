a61d40d10b987b35e015d57ba69ad0c3716cb1b6
leesuwon0673 <lsw0398@naver.com>
2024-04-08 10:13:41 +0900

    💄 Update 내정보관리 UI개선
    전체적인 화면과 textLayout에 background 적용
    프로필 수정 UI 수정
    회원정보 UI 수정  Related to : #6

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/myPageManagement/MyPageManagementUserInfoModifyFragment.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/myPageManagement/MyPageManagementUserInfoModifyFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import kr.co.lion.farming_customer.MyPageManagementName
 import kr.co.lion.farming_customer.R
@@ -23,6 +24,8 @@ class MyPageManagementUserInfoModifyFragment : Fragment() {
 
         settingToolbar()
         settingButtonDone()
+        errorMessagePw()
+
 
         return fragmentMyPageManagementUserInfoModifyBinding.root
     }
@@ -46,6 +49,34 @@ class MyPageManagementUserInfoModifyFragment : Fragment() {
             buttonMyPageManagementUserInfoModifyDone.setOnClickListener {
                 myPageManagementActivity.removeFragment(MyPageManagementName.MY_PAGE_MANAGEMENT_USER_INFO_MODIFY)
                 myPageManagementActivity.removeFragment(MyPageManagementName.MY_PAGE_MANAGEMENT_CHECK_USER_INFO_MODIFY)
+            }
+        }
+    }
+
+    // 비밀번호 오류 메세지
+    fun errorMessagePw(){
+        fragmentMyPageManagementUserInfoModifyBinding.apply {
+            // 첫번째 비밀번호 입력할 때 오류 메세지
+            textFieldMyPageManagementUserInfoModifyUserPw.addTextChangedListener {
+                if(textFieldMyPageManagementUserInfoModifyUserPw.length() <6 || textFieldMyPageManagementUserInfoModifyUserPw.length() > 20){
+                    textInputLayoutUserPw.error = "6자 이상 20자 이하로 입력하세요"
+                }
+                else{
+                    textInputLayoutUserPw.error = null
+                    textInputLayoutUserPw.isErrorEnabled = false
+                }
+            }
+            // 두번째 비밀번호가 첫번째 비밀번호와 다를 때
+            textFieldMyPageManagementUserInfoModifyCheckUserPw.addTextChangedListener {
+                val userPw = textFieldMyPageManagementUserInfoModifyUserPw.text.toString()
+                val userPw2 = textFieldMyPageManagementUserInfoModifyCheckUserPw.text.toString()
+                if(userPw != userPw2){
+                    textInputLayoutUserPw2.error = "비밀번호가 다릅니다"
+                }
+                else{
+                    textInputLayoutUserPw2.error = null
+                    textInputLayoutUserPw2.isErrorEnabled = false
+                }
             }
         }
     }
