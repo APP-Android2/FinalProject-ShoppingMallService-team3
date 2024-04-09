d27e7f873083ad92112922936d5baca9c44def56
leesuwon0673 <lsw0398@naver.com>
2024-04-09 17:57:55 +0900

    💄 Add 좋아요 UI 구현
    좋아요 UI 구현했습니다. Related to : #15

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/LikeFragment.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/LikeFragment.kt
@@ -1,23 +1,25 @@
 package kr.co.lion.farming_customer.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ToggleButton
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.divider.MaterialDividerItemDecoration
+import kr.co.lion.farming_customer.GridSpaceItemDecoration
 import kr.co.lion.farming_customer.LikeType
 import kr.co.lion.farming_customer.R
 import kr.co.lion.farming_customer.activity.MainActivity
 import kr.co.lion.farming_customer.databinding.FragmentLikeBinding
 import kr.co.lion.farming_customer.databinding.RowLikeCropBinding
+import kr.co.lion.farming_customer.databinding.RowLikeFarmActivityBinding
 import kr.co.lion.farming_customer.databinding.RowLikePostBinding
+import kr.co.lion.farming_customer.databinding.RowLikeRentalBinding
 import kr.co.lion.farming_customer.viewmodel.LikeViewModel
 
 class LikeFragment : Fragment() {
@@ -25,12 +27,19 @@ class LikeFragment : Fragment() {
     lateinit var mainActivity: MainActivity
 
     lateinit var likeViewModel : LikeViewModel
+
+    lateinit var deco:MaterialDividerItemDecoration
+    lateinit var deco2:GridSpaceItemDecoration
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentLikeBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_like, container, false)
         likeViewModel = LikeViewModel()
         fragmentLikeBinding.likeViewModel = likeViewModel
         fragmentLikeBinding.lifecycleOwner = this
         mainActivity = activity as MainActivity
+
+        deco = MaterialDividerItemDecoration(mainActivity, MaterialDividerItemDecoration.VERTICAL)
+        deco2 = GridSpaceItemDecoration(2,40,-10,-10,-10)
 
         settingUI()
         settingToggleButton()
@@ -42,7 +51,6 @@ class LikeFragment : Fragment() {
     // 초기 설정
     fun settingUI(){
         likeViewModel.settingLikeType(LikeType.CROP)
-        settingRecyclerViewLikeCrop()
     }
 
     // toggleButton 선택에 따라 recyclerView가 바뀌는 메서드
@@ -50,23 +58,50 @@ class LikeFragment : Fragment() {
         fragmentLikeBinding.apply {
             toggleGroupLikeType.addOnButtonCheckedListener { group, checkedId, isChecked ->
                 if(isChecked){
-                    when(likeViewModel?.toggleLikeType?.value){
+                    when(checkedId){
                         R.id.buttonLikeCrop -> {
+                            recyclerViewLike.visibility = View.VISIBLE
+                            recyclerViewPost.visibility = View.GONE
+                            recyclerViewRental.visibility = View.GONE
                             settingRecyclerViewLikeCrop()
                         }
                         R.id.buttonLikePost -> {
+                            recyclerViewLike.visibility = View.GONE
+                            recyclerViewPost.visibility = View.VISIBLE
+                            recyclerViewRental.visibility = View.GONE
                             settingRecyclerViewLikePost()
+                        }
+                        R.id.buttonLikeFarm -> {
+                            recyclerViewLike.visibility = View.VISIBLE
+                            recyclerViewPost.visibility = View.GONE
+                            recyclerViewRental.visibility = View.GONE
+                            settingRecyclerViewLikeFarmAndActivity()
+                        }
+                        R.id.buttonLikeActivity -> {
+                            recyclerViewLike.visibility = View.VISIBLE
+                            recyclerViewPost.visibility = View.GONE
+                            recyclerViewRental.visibility = View.GONE
+                            settingRecyclerViewLikeFarmAndActivity()
+                        }
+                        R.id.buttonLikeRental -> {
+                            recyclerViewLike.visibility = View.GONE
+                            recyclerViewPost.visibility = View.GONE
+                            recyclerViewRental.visibility = View.VISIBLE
+                            settingRecyclerViewLikeRental()
                         }
                     }
                 }
             }
-
         }
     }
     // 농산물 recyclerView 설정
     fun settingRecyclerViewLikeCrop(){
         fragmentLikeBinding.apply {
             recyclerViewLike.apply {
+                removeItemDecoration(deco)
+                if(itemDecorationCount == 0){
+                    addItemDecoration(deco2)
+                }
                 adapter = LikeCropRecyclerViewAdapter()
                 layoutManager = GridLayoutManager(mainActivity,2)
 
@@ -74,13 +109,42 @@ class LikeFragment : Fragment() {
         }
     }
 
+    // 게시판 recyclerView 설정
     fun settingRecyclerViewLikePost(){
         fragmentLikeBinding.apply {
-            recyclerViewLike.apply {
+            recyclerViewPost.apply {
+                removeItemDecoration(deco2)
+
                 adapter = LikePostRecyclerViewAdapter()
                 layoutManager = LinearLayoutManager(mainActivity)
-                val deco = MaterialDividerItemDecoration(mainActivity, MaterialDividerItemDecoration.VERTICAL)
-                addItemDecoration(deco)
+                if(recyclerViewPost.itemDecorationCount == 0){
+                    addItemDecoration(deco)
+                }
+            }
+        }
+    }
+
+    // 주말농장 및 체험활동 recyclerView 설정
+    fun settingRecyclerViewLikeFarmAndActivity(){
+        fragmentLikeBinding.apply {
+            recyclerViewLike.apply {
+                removeItemDecoration(deco)
+                if(itemDecorationCount == 0){
+                    addItemDecoration(deco2)
+                }
+                adapter = LikeFarmAndActivityRecyclerViewAdapter()
+                layoutManager = GridLayoutManager(mainActivity, 2)
+            }
+        }
+    }
+
+    // 농기구 recyclerView 설정
+    fun settingRecyclerViewLikeRental(){
+        fragmentLikeBinding.apply {
+            recyclerViewRental.apply {
+                removeItemDecoration(deco)
+                adapter = LikeRentalRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(mainActivity)
             }
         }
     }
@@ -115,8 +179,6 @@ class LikeFragment : Fragment() {
         }
     }
 
-    // 게시판 recyclerView 설정
-
     // 게시판 recyclerView의 Adapter
     inner class LikePostRecyclerViewAdapter:RecyclerView.Adapter<LikePostRecyclerViewAdapter.LikePostViewHolder>(){
         inner class LikePostViewHolder(rowLikePostBinding: RowLikePostBinding):RecyclerView.ViewHolder(rowLikePostBinding.root){
@@ -142,7 +204,73 @@ class LikeFragment : Fragment() {
         }
 
         override fun onBindViewHolder(holder: LikePostViewHolder, position: Int) {
-            holder.rowLikePostBinding.textView.text = "게시판 리스트 $position"
+            holder.rowLikePostBinding.textViewLikePostLabel.text = "정보"
+            holder.rowLikePostBinding.textViewLikePostTitle.text = "글 제목$position"
+            holder.rowLikePostBinding.textViewLikePostContent.text = "글 내용입니다요 $position"
+            holder.rowLikePostBinding.textViewLikePostViewCnt.text = "$position"
+            holder.rowLikePostBinding.textViewLikePostCommentCnt.text = "$position"
+            holder.rowLikePostBinding.textViewLikePostDate.text = "2024-04-09"
+        }
+    }
+
+    // 주말농장 및 체험활동 recyclerView의 Adapter
+    inner class LikeFarmAndActivityRecyclerViewAdapter:RecyclerView.Adapter<LikeFarmAndActivityRecyclerViewAdapter.LikeFarmAndAcitivityViewHolder>(){
+        inner class LikeFarmAndAcitivityViewHolder(rowLikeFarmActivityBinding: RowLikeFarmActivityBinding):RecyclerView.ViewHolder(rowLikeFarmActivityBinding.root){
+            val rowLikeFarmActivityBinding:RowLikeFarmActivityBinding
+
+            init {
+                this.rowLikeFarmActivityBinding = rowLikeFarmActivityBinding
+                this.rowLikeFarmActivityBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LikeFarmAndAcitivityViewHolder {
+            val rowLikeFarmActivityBinding = RowLikeFarmActivityBinding.inflate(layoutInflater)
+            val likeFarmAndActivityViewHolder = LikeFarmAndAcitivityViewHolder(rowLikeFarmActivityBinding)
+            return likeFarmAndActivityViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 9
+        }
+
+        override fun onBindViewHolder(holder: LikeFarmAndAcitivityViewHolder, position: Int) {
+            holder.rowLikeFarmActivityBinding.textViewLikeFarmAndActivityName.text = "농작물이름$position"
+            holder.rowLikeFarmActivityBinding.textViewLikeFarmAndActivityLocation.text = "00광역시 00구 00면 $position"
+            holder.rowLikeFarmActivityBinding.textViewLikeFarmAndActivityPrice.text = "${position}0,000원"
+        }
+    }
+
+    // 농기구 recyclerView의 Adapter
+    inner class LikeRentalRecyclerViewAdapter:RecyclerView.Adapter<LikeRentalRecyclerViewAdapter.LikeRentalViewHolder>(){
+        inner class LikeRentalViewHolder(rowLikeRentalBinding: RowLikeRentalBinding):RecyclerView.ViewHolder(rowLikeRentalBinding.root){
+            val rowLikeRentalBinding:RowLikeRentalBinding
+
+            init {
+                this.rowLikeRentalBinding = rowLikeRentalBinding
+                this.rowLikeRentalBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LikeRentalViewHolder {
+            val rowLikeRentalBinding = RowLikeRentalBinding.inflate(layoutInflater)
+            val likeRentalViewHolder = LikeRentalViewHolder(rowLikeRentalBinding)
+            return likeRentalViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+        override fun onBindViewHolder(holder: LikeRentalViewHolder, position: Int) {
+            holder.rowLikeRentalBinding.textViewLikeRentalName.text = "농기구점 이름$position"
+            holder.rowLikeRentalBinding.textViewLikeRentalPhoneNumber.text = "010-1111-111$position"
+            holder.rowLikeRentalBinding.textViewLikeRentalAddress.text = "00광역시 00구 00면 $position"
         }
     }
 }
