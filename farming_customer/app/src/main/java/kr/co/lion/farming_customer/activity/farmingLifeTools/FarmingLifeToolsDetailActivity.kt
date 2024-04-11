477e6ecbaff3c5d5b2263c206b5b0f8ef69e60bd
Jieun <lje2000lje@naver.com>
2024-04-11 17:02:48 +0900

    :sparkles: Add 농기구 지도 연결, 리스트 UI 구현
    Related to : #27

---
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/activity/farmingLifeTools/FarmingLifeToolsDetailActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.lion.farming_customer.activity.farmingLifeTools
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.lion.farming_customer.R
+
+class FarmingLifeToolsDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_farming_life_tools_detail)
+    }
+}
---
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/famingLifeTools/adapter/FarmingLifeListRVAdapter.kt
@@ -0,0 +1,4 @@
+package kr.co.lion.farming_customer.fragment.famingLifeTools.adapter
+
+class FarmingLifeListRVAdapter {
+}
