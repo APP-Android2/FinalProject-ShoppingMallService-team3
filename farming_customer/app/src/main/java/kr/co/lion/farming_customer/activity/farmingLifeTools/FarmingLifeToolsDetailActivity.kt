4f19a8f07b4a31926fac3109b5973c5b99217114
Jieun <lje2000lje@naver.com>
2024-04-12 10:31:29 +0900

    :sparkles: Add 농기구 상세 하트 버튼 클릭 리스너 추가
    Related to : #27

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/activity/farmingLifeTools/FarmingLifeToolsDetailActivity.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/activity/farmingLifeTools/FarmingLifeToolsDetailActivity.kt
@@ -2,6 +2,7 @@ package kr.co.lion.farming_customer.activity.farmingLifeTools
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.lion.farming_customer.R
 import kr.co.lion.farming_customer.activity.farmingLifeTools.adapter.ToolsNameRVAdapter
@@ -10,6 +11,8 @@ import kr.co.lion.farming_customer.databinding.ActivityFarmingLifeToolsDetailBin
 class FarmingLifeToolsDetailActivity : AppCompatActivity() {
     lateinit var binding: ActivityFarmingLifeToolsDetailBinding
     lateinit var toolsNameRVAdapter: ToolsNameRVAdapter
+    private var isHeartBtn : Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFarmingLifeToolsDetailBinding.inflate(layoutInflater)
@@ -19,6 +22,7 @@ class FarmingLifeToolsDetailActivity : AppCompatActivity() {
         binding.farmingLifeToolsDetailToolRv.layoutManager = LinearLayoutManager(applicationContext)
 
         settingDetailToolbar()
+        settingHeartBtn()
 
         setContentView(binding.root)
     }
@@ -29,6 +33,20 @@ class FarmingLifeToolsDetailActivity : AppCompatActivity() {
                 setNavigationOnClickListener {
                     finish()
                 }
+            }
+        }
+    }
+
+    fun settingHeartBtn() {
+        binding.farmingLifeToolsDetailHeartBtn.setOnClickListener {
+            // 상태 변경
+            isHeartBtn = !isHeartBtn
+
+            if (isHeartBtn) {
+                binding.farmingLifeToolsDetailHeartBtn.setImageResource(R.drawable.heart_02)
+            }
+            else {
+                binding.farmingLifeToolsDetailHeartBtn.setImageResource(R.drawable.heart_01)
             }
         }
     }
