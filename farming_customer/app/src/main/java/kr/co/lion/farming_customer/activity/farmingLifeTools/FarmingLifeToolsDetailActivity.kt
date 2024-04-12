befb97901e4570ed17b27d5ab88b6f5ceaec91f4
Jieun <lje2000lje@naver.com>
2024-04-12 10:16:06 +0900

    :lipstick: Add 농기구 리스트 상세화면 UI 구현, 보유 농기구 어댑터 연결
    Related to : #27

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/activity/farmingLifeTools/FarmingLifeToolsDetailActivity.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/activity/farmingLifeTools/FarmingLifeToolsDetailActivity.kt
@@ -2,14 +2,21 @@ package kr.co.lion.farming_customer.activity.farmingLifeTools
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.lion.farming_customer.R
+import kr.co.lion.farming_customer.activity.farmingLifeTools.adapter.ToolsNameRVAdapter
 import kr.co.lion.farming_customer.databinding.ActivityFarmingLifeToolsDetailBinding
 
 class FarmingLifeToolsDetailActivity : AppCompatActivity() {
     lateinit var binding: ActivityFarmingLifeToolsDetailBinding
+    lateinit var toolsNameRVAdapter: ToolsNameRVAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFarmingLifeToolsDetailBinding.inflate(layoutInflater)
+
+        toolsNameRVAdapter = ToolsNameRVAdapter()
+        binding.farmingLifeToolsDetailToolRv.adapter = toolsNameRVAdapter
+        binding.farmingLifeToolsDetailToolRv.layoutManager = LinearLayoutManager(applicationContext)
 
         settingDetailToolbar()
 
