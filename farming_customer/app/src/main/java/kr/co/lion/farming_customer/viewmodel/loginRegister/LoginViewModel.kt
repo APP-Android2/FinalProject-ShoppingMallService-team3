384831c7a937de747eb32f136079559ce2189e9e
Kyung Soo Lee <blueeye0909@naver.com>
2024-04-16 14:25:12 +0900

    :sparkles: Feat Login ViewModel 연결
    fragment_login.xml과 LoginViewModel을 연결했습니다.
    
    related to: #49

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/viewmodel/loginRegister/LoginViewModel.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/viewmodel/loginRegister/LoginViewModel.kt
@@ -1,4 +1,11 @@
 package kr.co.lion.farming_customer.viewmodel.loginRegister
 
-class LoginViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+    // 아이디
+    val textFieldLoginId = MutableLiveData<String>()
+    // 비밀번호
+    val textFieldLoginPw = MutableLiveData<String>()
 }
