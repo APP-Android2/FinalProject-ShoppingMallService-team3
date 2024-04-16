ea5c2cc72e065bf0840db845a5b59a6ffe047460
Kyung Soo Lee <blueeye0909@naver.com>
2024-04-16 14:54:50 +0900

    :sparkles: Feat Register ViewModel 연결
    fragment_register.xml과 RegisterViewModel을 연결했습니다.
    
    related to: #49

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/viewmodel/loginRegister/RegisterViewModel.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/viewmodel/loginRegister/RegisterViewModel.kt
@@ -1,4 +1,21 @@
 package kr.co.lion.farming_customer.viewmodel.loginRegister
 
-class RegisterViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // 서비스 이용 약관 상태 관리
+    val isServiceTermChecked = MutableLiveData<Boolean>()
+    // 개인정보 수집 및 이용 안내 상태 관리
+    val isPersonalInfoTermChecked = MutableLiveData<Boolean>()
+    // 알림 서비스 수락
+    val isAlertServiceTermChecked = MutableLiveData<Boolean>()
+
+    init {
+        // 초기값 설정
+        isServiceTermChecked.value = false
+        isPersonalInfoTermChecked.value = false
+        isAlertServiceTermChecked.value = false
+    }
+
 }
