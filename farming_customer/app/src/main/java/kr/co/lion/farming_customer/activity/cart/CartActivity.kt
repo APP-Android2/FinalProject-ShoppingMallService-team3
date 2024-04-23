5f55f91a1cfb52f544f23cf6251b00648b2fa73a
Kyung Soo Lee <blueeye0909@naver.com>
2024-04-23 13:21:23 +0900

    :sparkles: Update CartActivity
    - CartActivity에 농산품 번호, 옵션명, 옵션 개수를 넘겨 받아 CartFragment로 넘겨줌
    
    related to: #59

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/activity/cart/CartActivity.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/activity/cart/CartActivity.kt
@@ -15,7 +15,7 @@ import kr.co.lion.farming_customer.fragment.cart.CartTabCropFragment
 import kr.co.lion.farming_customer.fragment.cart.CartTabFarmFragment
 
 class CartActivity : AppCompatActivity() {
-    lateinit var activityCartBinding: ActivityCartBinding
+    private lateinit var activityCartBinding: ActivityCartBinding
 
     // 프래그먼트 객체를 담을 변수
     var oldFragment: Fragment? = null
@@ -27,7 +27,12 @@ class CartActivity : AppCompatActivity() {
         activityCartBinding = ActivityCartBinding.inflate(layoutInflater)
         setContentView(activityCartBinding.root)
 
-        replaceFragment(CartFragmentName.CART_FRAGMENT, false, false, null)
+        // Intent에서 데이터 추출
+        val cropIdx = intent.getIntExtra("crop_idx", -1)
+        val optionName = intent.getStringExtra("optionName")
+        val optionCnt = intent.getIntExtra("optionCnt", 0)
+
+        getCrop(cropIdx, optionName, optionCnt)
     }
 
     fun replaceFragment(name: CartFragmentName, addToBackStack:Boolean, isAnimate:Boolean, data:Bundle?, container:Int = R.id.containerCart){
@@ -40,18 +45,21 @@ class CartActivity : AppCompatActivity() {
         }
         // 이름으로 분기한다.
         // Fragment의 객체를 생성하여 변수에 담아준다.
-        when(name){
+        newFragment = when(name){
             CartFragmentName.CART_FRAGMENT -> {
-                newFragment = CartFragment()
+                CartFragment()
             }
+
             CartFragmentName.CART_TAB_CROP_FRAGMENT -> {
-                newFragment = CartTabCropFragment()
+                CartTabCropFragment()
             }
+
             CartFragmentName.CART_TAB_FARM_FRAGMENT -> {
-                newFragment = CartTabFarmFragment()
+                CartTabFarmFragment()
             }
+
             CartFragmentName.CART_TAB_ACTIVITY_FRAGMENT -> {
-                newFragment = CartTabActivityFragment()
+                CartTabActivityFragment()
             }
         }
         if(data != null){
@@ -113,5 +121,21 @@ class CartActivity : AppCompatActivity() {
         // 지정한 이름으로 있는 Fragment를 BackStack에서 제거한다.
         SystemClock.sleep(200)
         supportFragmentManager.popBackStack(name.str, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    }
+
+    // 농작물 정보를 받아온다.
+    private fun getCrop(cropIdx: Int, optionName: String?, optionCnt: Int){
+        // 받아온 정보를 Bundle에 담아 Fragment로 전달해준다.
+        // 이 때, 각 변수에는 기본 값이 아닌 받아온 값이 저장되어 있어야 한다.
+        if (cropIdx != -1 && !optionName.isNullOrEmpty() && optionCnt != 0){
+            val bundle = Bundle()
+            bundle.apply {
+                putInt("cropIdx", cropIdx)
+                putString("optionName", optionName)
+                putInt("optionCnt", optionCnt)
+            }
+            replaceFragment(CartFragmentName.CART_FRAGMENT, addToBackStack = false,
+                isAnimate = false, data = bundle)
+        }
     }
 }
