a2fb75179fdea52873e0513a2cae4b02279eb4c5
Kyung Soo Lee <blueeye0909@naver.com>
2024-04-23 13:32:26 +0900

    :sparkles: Update CartFragment
    - CartFragment에 농산품 번호, 옵션명, 옵션 개수를 넘겨 받아 CartTabCropFragment로 넘겨줌
    
    related to: #59

--- farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/cart/CartFragment.kt
+++ farming_customer/app/src/main/java/kr/co/lion/farming_customer/fragment/cart/CartFragment.kt
@@ -12,16 +12,15 @@ import kr.co.lion.farming_customer.activity.cart.CartActivity
 import kr.co.lion.farming_customer.databinding.FragmentCartBinding
 
 class CartFragment : Fragment() {
-    lateinit var fragmentCartBinding: FragmentCartBinding
+    private lateinit var fragmentCartBinding: FragmentCartBinding
     lateinit var cartActivity: CartActivity
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
         fragmentCartBinding = FragmentCartBinding.inflate(inflater)
         cartActivity = activity as CartActivity
 
         settingToolbar()
         settingTabLayoutCart()
-        cartActivity.replaceFragment(CartFragmentName.CART_TAB_CROP_FRAGMENT, false, false, null, container = R.id.containerCartTab)
 
         return fragmentCartBinding.root
     }
@@ -39,22 +38,48 @@ class CartFragment : Fragment() {
     }
 
     // 탭 설정
-    fun settingTabLayoutCart() {
+    private fun settingTabLayoutCart() {
         fragmentCartBinding.apply {
             tabLayoutCart.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
                 override fun onTabSelected(tab: TabLayout.Tab?) {
                     when(tab!!.position){
                         // 농산물 탭
                         0 -> {
-                            cartActivity.replaceFragment(CartFragmentName.CART_TAB_CROP_FRAGMENT, false, false, null, R.id.containerCartTab)
+                            val cropIdx = arguments?.getInt("cropIdx")!!
+                            val optionName = arguments?.getString("optionName")
+                            val optionCnt = arguments?.getInt("optionCnt")!!
+
+                            val bundle = Bundle()
+                            bundle.apply {
+                                putInt("cropIdx", cropIdx)
+                                putString("optionName", optionName)
+                                putInt("optionCnt", optionCnt)
+                            }
+
+                            cartActivity.replaceFragment(CartFragmentName.CART_TAB_CROP_FRAGMENT,
+                                addToBackStack = false,
+                                isAnimate = false,
+                                data = bundle,
+                                container = R.id.containerCartTab
+                            )
                         }
                         // 주말농장 탭
                         1 -> {
-                            cartActivity.replaceFragment(CartFragmentName.CART_TAB_FARM_FRAGMENT, false, false, null, R.id.containerCartTab)
+                            cartActivity.replaceFragment(CartFragmentName.CART_TAB_FARM_FRAGMENT,
+                                addToBackStack = false,
+                                isAnimate = false,
+                                data = null,
+                                container = R.id.containerCartTab
+                            )
                         }
                         // 체험활동 탭
                         2 -> {
-                            cartActivity.replaceFragment(CartFragmentName.CART_TAB_ACTIVITY_FRAGMENT, false, false, null, R.id.containerCartTab)
+                            cartActivity.replaceFragment(CartFragmentName.CART_TAB_ACTIVITY_FRAGMENT,
+                                addToBackStack = false,
+                                isAnimate = false,
+                                data = null,
+                                container = R.id.containerCartTab
+                            )
                         }
                     }
                 }
